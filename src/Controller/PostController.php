962850bb8974e9bbfb397b3cbf9d486fad9c8edc
Dominik <dominik.koralewski@gmail.com>
2020-12-02 21:05:16 +0100

    forms added

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,23 +3,45 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 /**
  * @Route("/post", name="post.")
  */
 class PostController extends AbstractController
 {
+
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+        $session1 = new Session();
+        $session1->start();
+    }
+
     /**
      * @Route("/", name="index")
+     * @param PostRepository $postRepository
+     * @return Response
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
+
+//        $session = new Session();
+//        $session->start();
+
+        $posts = $postRepository->findAll();
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $posts
         ]);
     }
 
@@ -28,21 +50,64 @@ class PostController extends AbstractController
      * @param Request $request
      * @return Response
      */
-    public function create(Request $request){
+    public function create(Request $request)
+    {
         // create a new post with a title
         $post = new Post();
 
-        $post->setTitle('This is going to be a title');
+        $form = $this->createForm(PostType::class, $post);
 
+        $form->handleRequest($request);
 
-        //entity manager
-        $em = $this->getDoctrine()->getManager();
+        if ($form->isSubmitted()) {
+            //entity manager
 
-        $em->persist($post);
-        $em->flush();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('post.index'));
+        }
+
 
         //return a response
-        return new Response('Post was created');
+        return $this->render('post/create.html.twig', [
+            'form' => $form->createView()
+        ]);
 
+    }
+
+    /**
+     * @Route("/show/{id}", name="show")
+     * @param Post $post
+     * @return Response
+     */
+    public function show(Post $post)
+    {
+        //^Can be done also like: $post = $postRepository->find($id), with arguments ($id, PostRepository $postRepository)
+
+        //create show view
+        return $this->render('post/show.html.twig', [
+            'post' => $post
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     * @param Post $post
+     * @return RedirectResponse
+     */
+    public function remove(Post $post)
+    {
+
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->remove($post);
+        $em->flush();
+
+        $this->addFlash('success', 'Post was removed');
+
+        return $this->redirect($this->generateUrl('post.index'));
     }
 }
